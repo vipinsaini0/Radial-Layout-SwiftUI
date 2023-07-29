7bde196a10cef627b30d86885cfb9c7d51ee1b2f
Vipin Saini <vipin.saini@jhavtech.com>
2023-07-29 11:54:57 +0530

    Initial Commit

---
+++ RadialView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RadialView
+//
+//  Created by Vipin Saini on 29-07-2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RadialView/RadialViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadialViewApp.swift
+//  RadialView
+//
+//  Created by Vipin Saini on 29-07-2023.
+//
+
+import SwiftUI
+
+@main
+struct RadialViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
